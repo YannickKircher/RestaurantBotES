91f0db4d603756aa7031d27a1e784e85c20fbaa9
YannickKircher <kircheryannick@gmail.com>
2023-06-26 15:53:25 +0200

    added comments, default user location and openness, -> reworked queries

--- src/intenthandler.py
+++ src/intenthandler.py
@@ -4,31 +4,46 @@ from json_return import return_text, return_card
 from query import CustomQuery
 
 default_user_price_range = "low"
+default_user_location = "Matera"
+
+default_user_openness = 4 
 
 def restaurant_intent_handler(request_json):
-    client = bigquery.Client()
     
+    default_user_price_range = "low"
+    default_user_location = "Matera"
+    default_user_openness = 4 
+    
+    
+    client = bigquery.Client()
+ 
+    # if a dish was given as a parameter, return the best rated restaurant with that dish on the menu and a matching location
     if len(request_json["queryResult"]["parameters"]["dish"]) > 0 :
-        query = "SELECT * FROM `restaurantguide-9oyv.restaurant_guide.restaurants_random` ORDER BY rating DESC"
-        query_job = client.query(query)
+        query = CustomQuery(table_name="`restaurantguide-9oyv.restaurant_guide.restaurants_random`",order_by_list=["rating"])
+        #check for location preference and add a where statement if found
+        if request_json["queryResult"]["parameters"]["geo-city"] != "None":
+            query.add_where({'location': f' = "{request_json["queryResult"]["parameters"]["geo-city"]}"'})
+        
+        query_job = client.query(str(query))
+        #query.clear()
         query_df = DataFrame([dict(row) for row in query_job])
         
         
         #select first (best rated) restaurant with the dish on the menu
-        # workaround because I'm not using NoSQL 
+        # WORKAROUND BECAUSE I AM NOT USING NoSQL <--------
         for dish in request_json["queryResult"]["parameters"]["dish"]:
             restaurant_list = [item for item in query_df.iterrows() 
                                 if dish in item[1]["menu_list"] or dish.capitalize() in item[1]["menu_list"]]
             break
         
         #found no restaurant with that item on the menu 
-        if len(restaurant_list)==0:
+        if len(restaurant_list) == 0:
             return return_text(
                 f"""I'm sorry, I could not find any Restaurant with the dishes {
                     ', '.join(request_json["queryResult"]["parameters"]["dish"])
                     } on the Menu""")
 
-        else: #found at least 1 
+        else: #found at least 1 pick the best rated one
             restaurant = restaurant_list[0][1]
             return return_card(title = restaurant["name"],
                                 subtitle = str([
@@ -41,22 +56,33 @@ def restaurant_intent_handler(request_json):
                                 btn_url = restaurant["img_url"],
                                 btn_text = "go to Restaurant web page"
                                 )
-        
+    
+    # query for a restaurant with the given parameters except for the dish
     else:
-        try:
+        try: 
             query = CustomQuery(table_name="`restaurantguide-9oyv.restaurant_guide.restaurants_random`",order_by_list=["rating"])
             #check for price preference and add a where statement if found
             if request_json["queryResult"]["parameters"]["price_range"] != "None":
                 query.add_where({f'price_range' : f'= "{request_json["queryResult"]["parameters"]["price_range"].lower()}"'})
             
-            #check for cuisin preference and add a where statement if found
+            #check for cuisine preference and add a where statement if found
             if request_json["queryResult"]["parameters"]["cuisine"] != "None":
                 query.add_where({'country': f' = "{request_json["queryResult"]["parameters"]["cuisine"].lower()}"'})
-        
-            query.limit = 4
+                
+            #check for location preference and add a where statement if found
+            if request_json["queryResult"]["parameters"]["geo-city"] != "None":
+                query.add_where({'location': f' = "{request_json["queryResult"]["parameters"]["geo-city"].lower()}"'})
+
+            #pick a random restaurant from the top n restaurants
+            #n is equal to the user openness
+            query.limit = default_user_openness
+            
+            #query
             query_job = client.query(str(query))
-            query.clear()
+            #query.clear()
             query_df = DataFrame([dict(row) for row in query_job])
+            
+            #pick on of the top n restaurants
             sample_df = query_df.sample()
             
 
@@ -66,15 +92,20 @@ def restaurant_intent_handler(request_json):
                             btn_url = sample_df["img_url"].values[0],
                             btn_text = "go to Restaurant web page"
                             )
-            
+        
+        # if the query returns no valid restraurant that fits the specifications, 
+        # -> return a restaurant, that matches default user preferences and the location if given else use default_user_location
         except KeyError:
-            global default_user_price_range
             query = CustomQuery(table_name="`restaurantguide-9oyv.restaurant_guide.restaurants_random`",
                                 where_statements={'price_range':f'= "{default_user_price_range}"'},
                                 order_by_list=["rating"], limit=10)
+            if request_json["queryResult"]["parameters"]["geo-city"] != "None":
+                query.add_where({'location': f' = "{request_json["queryResult"]["parameters"]["geo-city"]}"'})
+            else:
+                query.add_where({'location': f' = "{default_user_location}"'})
         
+            #query
             query_job = client.query(str(query))
-
             query_df = DataFrame([dict(row) for row in query_job])
             sample_df = query_df.sample()
             
