d6520e8f706b92fcb9e194832b812323a9126592
YannickKircher <kircheryannick@gmail.com>
2023-06-26 15:19:02 +0200

    added relevant dockstings

--- src/json_return.py
+++ src/json_return.py
@@ -1,4 +1,8 @@
+
 def return_card(title,subtitle,img_url,btn_url=None,btn_text=None):
+    """builds a json response with a card response, 
+    that includes a img and a button,
+    that can be shown by the dialogflow API to the user"""
     if (btn_url is not None) & (btn_text is not None): 
         return {
             "fulfillmentMessages": [
@@ -31,6 +35,8 @@ def return_card(title,subtitle,img_url,btn_url=None,btn_text=None):
             }
 
 def return_text(text_response):
+    """builds a json response with a text response, 
+    that can be shown by the dialogflow API to the user"""
     return {
         "fulfillmentMessages": [
             {
