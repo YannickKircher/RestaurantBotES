bc80019caa29ed9629d161dacb4899149b90d3fa
YannickKircher <kircheryannick@gmail.com>
2023-07-01 21:57:42 +0200

    removed test_text2

--- RecipeBotCX/src/json_return.py
+++ RecipeBotCX/src/json_return.py
@@ -13,7 +13,7 @@ def rich_text(texts:list,content_type="chips"):
 
 def json_recipe_data(recipe_id,recipe_name,ingredients,directions):
   
-  lines = [recipe_name]+["\n".join(["<b>Ingredients:</b>"]+ingredients)]+["\n".join(["<b>Directions:</b>"]+directions)]
+  lines = [recipe_name]+["\n".join(["Ingredients:"]+ingredients)]+["\n".join(["Directions:"]+directions)]
   
   return {
    "fulfillment_response": {
@@ -23,25 +23,6 @@ def json_recipe_data(recipe_id,recipe_name,ingredients,directions):
            "text": [line]
          }
        } for line in lines 
-     ]
-   },
-   "sessionInfo": {
-     "parameters": {
-          "recipe_id": recipe_id
-     }
-   }
- }
-
-
-def test_text2(recipe_id,recipe_name,ingredients):
-    return {
-   "fulfillment_response": {
-     "messages": [
-       {
-         "text": {
-           "text": "\n".join([recipe_name]+ingredients)
-         }
-       } 
      ]
    },
    "sessionInfo": {
